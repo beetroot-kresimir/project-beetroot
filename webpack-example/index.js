44a5063393a66c653aa99718047c76652e989eee
“beetroot-kresimir” <beetroot.academy.kresimir@gmail.com>
2024-02-15 22:02:42 +0100

    add event exercise, and webpack-example

---
+++ webpack-example/index.js
@@ -0,0 +1,4 @@
+import './styles.scss';
+
+const app = document.getElementById('app');
+app.innerHTML = `<h1>Hello Webpackkelellelelelel!</h1>`;
---
+++ webpack-example/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// final bundle code = generated/compiled/minifed/optised version
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+  },
+};
